679d33532ac41ad1b984cc6894a9c4af7caf8911
Hari <hari@nagarajan.io>
2024-05-20 17:40:06 -0400

    updates for nuxt3 upgrade

--- components/Intro.vue
+++ components/Intro.vue
@@ -12,117 +12,106 @@
           @HariRVA</a>
         <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
       </div>
-      <Hero/>
-
+      <Hero />
     </div>
   </div>
 </template>
 
-<script>
-  import Hero from '@/components/Hero'
-
-  export default {
-    components: {
-      Hero
-    },
-    name: "Intro"
-  }
+<script setup>
+import Hero from '@/components/Hero.vue'
 </script>
 
-<style>
+<style scoped>
+.intro {
+  display: block;
+  align-content: center;
+  align-items: center;
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 10vh;
+}
 
-  .intro {
-    display: block;
-    align-content: center;
-    align-items: center;
-    width: 80%;
-    margin-left: auto;
-    margin-right: auto;
-    padding-top: 10vh;
+.introRow {
+  width: 100%;
+  display: flex;
+  box-sizing: border-box;
+  padding: 0;
+  flex-wrap: wrap;
+}
+
+.introCol {
+  max-width: 50%;
+  flex-basis: 50%;
+  -webkit-font-smoothing: antialiased;
+  -moz-font-smoothing: antialiased;
+  -o-font-smoothing: antialiased;
+  font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
+}
+
+.introCol h1 {
+  font-size: 48px;
+  font-weight: 600;
+  color: ghostwhite;
+  letter-spacing: 1px;
+  text-shadow: 0 1px 2px rgba(0, 0, 0, .2);
+  margin: 0;
+}
+
+.introCol h2 {
+  margin: 0;
+  color: wheat;
+  text-shadow: 0 1px 2px rgba(0, 0, 0, .2);
+  font-weight: 300;
+  text-transform: uppercase;
+  font-size: 16px;
+}
+
+.introCol p {
+  color: whitesmoke;
+  text-shadow: 0 1px 2px rgba(0, 0, 0, .2);
+  font-weight: 400;
+}
+
+.highlight {
+  color: red;
+}
+
+@media (max-width: 440px) {
+  .titles {
+    font-size: 24px;
+    line-height: 1.0em;
+    font-weight: 600;
+    color: white;
+    letter-spacing: 1px;
+    -webkit-text-stroke: 1px black;
   }
 
-  .introRow {
+  .intro {
+    padding-top: 5vh !important;
+    margin-top: 0vh;
     width: 100%;
-    display: flex;
-    box-sizing: border-box;
-    padding: 0;
-    flex-wrap: wrap;
   }
 
   .introCol {
+    max-width: 90%;
+  }
+
+  .introRow {
+    padding: 0em 0em 0em 0em;
+    flex-direction: column-reverse;
+    align-items: center;
+    max-width: 100%;
+  }
+
+  .photoCol {
     max-width: 50%;
-    flex-basis: 50%;
-    -webkit-font-smoothing: antialiased;
-    -moz-font-smoothing: antialiased;
-    -o-font-smoothing: antialiased;
-    font-smoothing: antialiased;
-    text-rendering: optimizeLegibility;
+    margin-left: 1em;
+    margin-right: 1em;
   }
 
-  .introCol h1 {
-    font-size: 48px;
-    font-weight: 600;
-    color: ghostwhite;
-    letter-spacing: 1px;
-    text-shadow: 0 1px 2px rgba(0, 0, 0, .2);
-    margin: 0;
+  .photoCol img {
   }
-
-  .introCol h2 {
-    margin: 0;
-    color: wheat;
-    text-shadow: 0 1px 2px rgba(0, 0, 0, .2);
-    font-weight: 300;
-    text-transform: uppercase;
-    font-size: 16px;
-  }
-
-  .introCol p {
-    color: whitesmoke;
-    text-shadow: 0 1px 2px rgba(0, 0, 0, .2);
-    font-weight: 400;
-  }
-
-  .highlight {
-    color: red;
-  }
-
-  @media (max-width: 440px) {
-    .titles {
-      font-size: 24px;
-      line-height: 1.0em;
-      font-weight: 600;
-      color: white;
-      letter-spacing: 1px;
-      -webkit-text-stroke: 1px black;
-    }
-
-    .intro {
-      padding-top: 5vh !important;
-      margin-top: 0vh;
-      width: 100%;
-    }
-
-    .introCol {
-      max-width: 90%;
-    }
-
-    .introRow {
-      padding: 0em 0em 0em 0em;
-      flex-direction: column-reverse;
-      align-items: center;
-      max-width: 100%;
-
-    }
-
-    .photoCol {
-      max-width: 50%;
-      margin-left: 1em;
-      margin-right: 1em;
-    }
-
-    .photoCol img {
-    }
-
-  }
+}
 </style>
